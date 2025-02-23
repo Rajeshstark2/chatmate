46b47d81abe3cb55e89e9f9f6ec909504cd04955
rajesh k <143821418+Rajeshstark2@users.noreply.github.com>
2025-02-23 20:15:52 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,25 +1,35 @@
+// server.js
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
 import userRoutes from './routes/user.routes.js';
 
 import connectToMongoDB from './db/connectToMongoDB.js';
-import { app, server } from './socket/socket.js';
+import { app, server  } from './socket/socket.js';
+
 
 const PORT = process.env.PORT || 5000;
 
-dotenv.config();
-app.use(express.json()); // to parse incoming JSON requests
-app.use(cookieParser());
+const __dirname = path.resolve();
 
-app.use("/api/auth", authRoutes);
-app.use('/api/messages', messageRoutes);
-app.use('/api/users', userRoutes);
+dotenv.config();
+app.use(express.json()); // to parse the incoming request with JSON payloads from req.body
+app.use(cookieParser()); 
+
+app.use("/api/auth",authRoutes)
+app.use('/api/messages',messageRoutes);
+app.use('/api/users',userRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
+});
 
 server.listen(PORT, () => {
-  connectToMongoDB();
-  console.log(`Server is running on port ${PORT}`);
+  connectToMongoDB()
+  console.log(`Server is running on ports ${PORT}`);
 });
